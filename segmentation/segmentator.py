d12d6bb15211a52d8daaa031ac28e6113aad4fed
Lukas Leindals <s183920@student.dtu.dk>
2023-06-09 14:57:58 +0200

    wandb logging

--- segmentation/segmentator.py
+++ segmentation/segmentator.py
@@ -8,6 +8,8 @@ from torch.utils.data import DataLoader
 import matplotlib.pyplot as plt
 import numpy as np
 from hparams import default_config
+import PIL
+import wandb
 
 
 """
@@ -84,8 +86,6 @@ class Segmentator(Agent):
     def train(self):
         # extract parameters from config
         num_epochs = self.config["train_kwargs"]["num_epochs"]
-        
-        X_test, Y_test = next(iter(self.test_loader))
 
         for epoch in range(num_epochs):
             print('* Epoch %d/%d' % (epoch+1, num_epochs))
@@ -98,33 +98,64 @@ class Segmentator(Agent):
                 # calculate metrics to show the user
                 avg_loss += loss / len(self.train_loader)
                 
-            print(' - loss: %f' % avg_loss)
+            
+            
+            if self.wandb_run is not None:
+                pil_image = self.test_images(validation = True)
+                example = wandb.Image(pil_image, caption=f"Epoch {epoch}")
+                self.wandb_run.log({
+                    "train_loss": avg_loss,
+                    "epoch": epoch,
+                    "example": example,
+                })
+            else:
+                print(' - loss: %f' % avg_loss)
 
-            # # show intermediate results
-            # model.eval()  # testing mode
-            # Y_hat = F.sigmoid(model(X_test.to(device))).detach().cpu()
-            # clear_output(wait=True)
-            # for k in range(6):
-            #     plt.subplot(2, 6, k+1)
-            #     plt.imshow(np.rollaxis(X_test[k].numpy(), 0, 3), cmap='gray')
-            #     plt.title('Real')
-            #     plt.axis('off')
+    def test_images(self, validation = True):
+        if validation:
+            X_test, Y_test = next(iter(self.val_loader))
+        else:
+            X_test, Y_test = next(iter(self.test_loader))
+        
+        # show intermediate results
+        self.model.eval()  # testing mode
+        Y_hat = F.sigmoid(self.model(X_test.to(self.device))).detach().cpu()
+        
+        fig, axes = plt.subplots(2, 6, figsize=(20, 5))
+        for k in range(6):
+            ax = axes[0, k]
+            ax.imshow(np.rollaxis(X_test[k].numpy(), 0, 3), cmap='gray')
+            ax.set_title('Real')
+            ax.set_axis_off()
 
-            #     plt.subplot(2, 6, k+7)
-            #     plt.imshow(Y_hat[k, 0], cmap='gray')
-            #     plt.title('Output')
-            #     plt.axis('off')
-            # plt.suptitle('%d / %d - loss: %f' % (epoch+1, epochs, avg_loss))
-            # plt.show()
+            ax = axes[1, k]
+            ax.imshow(Y_hat[k, 0], cmap='gray')
+            ax.set_title('Output')
+            ax.set_axis_off()
+        # plt.suptitle('%d / %d - loss: %f' % (epoch+1, num_epochs, avg_loss))
+        # plt.savefig("test.png")
+        # img = PIL.Image.frombytes('RGB', fig.canvas.get_width_height(),fig.canvas.tostring_rgb())
+        # img = from_canvas(fig)
+        import io
+        img_buf = io.BytesIO()
+        plt.savefig(img_buf, format='png')
+
+        img = PIL.Image.open(img_buf)
+            
+        return img
             
     def predict(self, data):
         self.model.eval()  # testing mode
         Y_pred = [F.sigmoid(self.model(X_batch.to(self.device))) for X_batch, _ in data]
         return np.array(Y_pred)
         
+def from_canvas(fig):
+    lst = list(fig.canvas.get_width_height())
+    lst.append(3)
+    return PIL.Image.fromarray(np.fromstring(fig.canvas.tostring_rgb(),dtype=np.uint8).reshape(lst))
 
 if __name__ == "__main__":
-    segmentator = Segmentator()
+    segmentator = Segmentator(use_wandb=True)
     segmentator.train()
     
     
