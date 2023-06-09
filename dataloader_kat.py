f13c32c706c2f24909e431fd291f7bfbd75ad3cc
Katrine B <s183910@student.dtu.dk>
2023-06-09 14:42:24 +0200

    DRIVE dataloader update

--- dataloader_kat.py
+++ dataloader_kat.py
@@ -10,40 +10,32 @@ import matplotlib.pyplot as plt
 from torch.utils.data import DataLoader
 import random
 import glob
+import PIL.Image as Image
 
-class HotdogDataset(datasets.ImageFolder):
-
+class DRIVE2(torch.utils.data.Dataset):
     def __init__(self, train = True, transform = None, data_augmentation = True, *args, **kwargs):
         # set datadir
         # self.datadir = 'hotdog/small_data/' + ('train' if train else 'test')
-        self.datadir = '/dtu/datasets1/02514/hotdog_nothotdog/' + ('train' if train else 'test')
+        self.data_path = '/dtu/datasets1/02514/DRIVE/' + ('training' if train else 'test')
+        # self.data_path = 'KATRINE_DATA/small_data/' + ('training' if train else 'test')
 
-        # set values
+        # self.data_path = '/dtu/datasets1/02514/DRIVE/training'
+        self.transform = transform if transform else self.default_transform
         self.img_size = (128, 128)
+
         self.train = train
         self.data_augmentation = data_augmentation
-        transform = transform if transform else self.default_transform
+        self.image_paths = sorted(glob.glob(self.data_path + '/images/*.tif'))
+        self.mask_paths = sorted(glob.glob(self.data_path + '/mask/*.gif'))
 
         # call super
-        super().__init__(self.datadir, transform=transform, *args, **kwargs)
+        # super().__init__(self.datadir, transform=transform, *args, **kwargs)
 
         # split train and val
-        if self.train:
-            self.train_subset, self.val_subset = torch.utils.data.random_split(
-        self, [0.8, 0.2], generator=torch.Generator().manual_seed(1))
 
-
-class PH2(torch.utils.data.Dataset):
-    def __init__(self, train = True, transform = None):
-        data_path = '/dtu/datasets1/02514/PH2_Dataset_images'
-
-        'Initialization'
-        self.transform = transform
-
-        self.image_paths = sorted(glob.glob(data_path + '/***/**_lesion/*.bmp'))
-
-
-
+    def default_transform(self):
+        return transforms.Compose([transforms.Resize((128,128)),
+                transforms.ToTensor()])
 
     def __len__(self):
         'Returns the total number of samples'
@@ -52,48 +44,47 @@ class PH2(torch.utils.data.Dataset):
     def __getitem__(self, idx):
         'Generates one sample of data'
         image_path = self.image_paths[idx]
-        # label_path = self.label_paths[idx]
+        mask_path = self.mask_paths[idx]
 
         image = Image.open(image_path)
-        # label = Image.open(label_path)
-        # Y = self.transform(label)
+        mask = Image.open(mask_path)
+        Y = self.transform(mask)
         X = self.transform(image)
-        return X
+        return X, Y
+
+    def get_subsets(self):
+        if self.train:
+            self.train_subset, self.val_subset, self.test_subset = torch.utils.data.random_split(
+        self, [0.6, 0.2, 0.2], generator=torch.Generator().manual_seed(1))
+
+
+
+
 
     @property
-    def default_transform(self):
-        if self.train and self.data_augmentation:
+    # def default_transform(self):
+    #     if self.train and self.data_augmentation:
 
-            return transforms.Compose([
-                transforms.Resize(self.img_size),
-                transforms.RandomRotation(random.randint(0,35)),
-                #transforms.ColorJitter(brightness=.5, hue=.3),
-                # transforms.RandomPerspective(distortion_scale=0.6, p = 0.4),
-                transforms.RandomHorizontalFlip(p=0.3),
-                transforms.RandomEqualize(),
-                transforms.ToTensor(),
-                # transforms.Normalize(mean=[self.mean[0], self.mean[1], self.mean[2]],
-                #                         std=[self.std[0], self.std[1], self.std[2]])
-                transforms.Normalize(mean = [0.5225634,0.44118169,0.35845589], std = [0.22521636,0.22928182,0.233647])
-            ])
-        else:
-            return transforms.Compose([
-                transforms.Resize(self.img_size),
-                transforms.ToTensor(),
-                # transforms.Normalize(mean=[self.mean[0], self.mean[1], self.mean[2]],
-                #                         std=[self.std[0], self.std[1], self.std[2]])
-                transforms.Normalize(mean = [0.5225634,0.44118169,0.35845589], std = [0.22521636,0.22928182,0.233647])
-            ])
+    #         return transforms.Compose([
+    #             transforms.Resize(self.img_size),
+    #             transforms.ToTensor()])
+    #     else:
+    #         return transforms.Compose([
+    #             transforms.Resize(self.img_size),
+    #             transforms.ToTensor()])
 
 
 
     def get_dataloader(self, batch_size = 32, *args, **kwargs):
-        if self.train:
-            train_loader = DataLoader(dataset=self.train_subset, shuffle=True, batch_size=batch_size, *args, **kwargs)
-            val_loader = DataLoader(dataset=self.val_subset, shuffle=False, batch_size=batch_size, *args, **kwargs)
-            return train_loader, val_loader
-        else:
-            return DataLoader(self, batch_size=batch_size, shuffle=False, *args, **kwargs)
+        # if self.train:
+        self.get_subsets()
+        train_loader = DataLoader(dataset=self.train_subset, shuffle=True, batch_size=batch_size, *args, **kwargs)
+        val_loader = DataLoader(dataset=self.val_subset, shuffle=False, batch_size=batch_size, *args, **kwargs)
+        test_loader = DataLoader(dataset=self.test_subset, shuffle=False, batch_size=batch_size, *args, **kwargs)
+
+        return train_loader, val_loader, test_loader
+        # else:
+        #     return DataLoader(self, batch_size=batch_size, shuffle=False, *args, **kwargs)
         # DataLoader(testset, batch_size=batch_size, shuffle=False)
 
 
@@ -101,16 +92,10 @@ class PH2(torch.utils.data.Dataset):
         return self.classes[label]
 
 
-
-
-
-
-
-
-
 if __name__ == "__main__":
-    dataset = HotdogDataset()
-    images, labels = next(iter(dataset.get_dataloader(batch_size=21, shuffle=False)))
+    dataset = DRIVE2()
+    train_loader, val_loader, test_loader = dataset.get_dataloader(batch_size=32, shuffle=True)
+    images, labels = next(iter(train_loader))
     plt.figure(figsize=(20,10))
 
     for i in range(21):
@@ -119,4 +104,4 @@ if __name__ == "__main__":
         plt.title(dataset.transform_label(labels[i].item()))
         plt.axis('off')
 
-    plt.savefig('hotdog_overview.png')
+    plt.savefig('DRIVE2_overview.png')
