115cecba959e2927e63a3b66f76b42f657429d33
Lukas Leindals <s183920@student.dtu.dk>
2023-06-14 15:29:08 +0200

    add latent explore

--- gan/test.sh
+++
@@ -1,3 +0,0 @@
-python code/stylegan2-ada-pytorch-main/projector.py --outdir=out --target=0.png \
-    --network=https://nvlabs-fi-cdn.nvidia.com/stylegan2-ada-pytorch/pretrained/ffhq.pkl \
-    --num-steps=10
