7b60ee5f9e288204f3c1acefab743aaa219431e8
Lukas Leindals <s183920@student.dtu.dk>
2023-06-14 12:40:14 +0200

    add test

--- gan/test.sh
+++ gan/test.sh
@@ -1,2 +1,3 @@
 python code/stylegan2-ada-pytorch-main/projector.py --outdir=out --target=0.png \
-    --network=https://nvlabs-fi-cdn.nvidia.com/stylegan2-ada-pytorch/pretrained/ffhq.pkl
+    --network=https://nvlabs-fi-cdn.nvidia.com/stylegan2-ada-pytorch/pretrained/ffhq.pkl \
+    --num-steps=10
